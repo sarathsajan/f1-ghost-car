487aae694108b4bf02855eba4edef53de90d1e23
unknown <sarathsajan1998@gmail.com>
2025-04-07 02:26:29 +0530

    after fetching telemtery data of driver, we plot it and view it from top which for obvious reasons forms the track map...seems like we are on right path

--- fetch_data.py
+++ fetch_data.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 import fastf1 as ff1
 import pandas as pd
 import json
@@ -20,3 +23,32 @@ single_driver_all_laps = session.laps.pick_drivers(DRIVER_ABBRV)
 single_driver_fastest_lap = single_driver_all_laps.pick_fastest()
 single_driver_fastest_lap.telemetry.to_csv(f'{DRIVER_ABBRV}_{YEAR}_{GP}_{SESSION_TYPE}_{str(single_driver_fastest_lap['LapTime']).replace(':','_').replace('0 days 00_','').replace('.','_')}.csv', index=False)
 
+
+# Sample data (replace with your actual coordinates)
+x = single_driver_fastest_lap.telemetry['X']
+y = single_driver_fastest_lap.telemetry['Y']
+z = single_driver_fastest_lap.telemetry['Z']
+
+# Create the figure and 3D axes
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection='3d')
+
+# Create the scatter plot
+ax.scatter(x, y, z, c='blue', marker='o')  # 'c' for color, 'marker' for point style
+
+# Set axis labels
+ax.set_xlabel('X-axis')
+ax.set_ylabel('Y-axis')
+ax.set_zlabel('Z-axis')
+
+# Set the view to be along the z-axis, this will give a top-down view
+ax.view_init(elev=90, azim=0)
+
+# Set title
+plt.title('3D Scatter Plot')
+
+# Save the plot
+plt.savefig(f'{DRIVER_ABBRV}_{YEAR}_{GP}_{SESSION_TYPE}_{str(single_driver_fastest_lap['LapTime']).replace(':','_').replace('0 days 00_','').replace('.','_')}.png')
+
+# Show the plot
+plt.show()
